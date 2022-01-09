395621cf866adfd8ac5bf7c61bda4f82bc99b8a4
Abdullah Mohamed <abdullahabdin@live.com>
2022-01-09 15:55:23 +0000

    update on server listening

--- node-demo.js
+++ node-demo.js
@@ -1,1 +1,12 @@
 
+var http = require('http')
+
+function requestHandler(request, response){
+
+console.log('Incoming request from' + request.url)
+response.end('hello from the node.js server.')
+
+}
+
+var server = http.createServer(requestHandler)
+server.listen(3000)
