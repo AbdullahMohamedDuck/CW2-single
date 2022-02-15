19210b6e41a0473241f4533d4f96421eebb9d4d9
Abdullah Mohamed <abdullahabdin@live.com>
2022-02-15 14:25:09 +0000

    heroku update

--- app.js
+++ app.js
@@ -109,5 +109,6 @@ app.use(function(req, res) {
     });
 
 //Heroku start port
-const port = process.env.PORT || 3000
-app.listen(port)
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  });
